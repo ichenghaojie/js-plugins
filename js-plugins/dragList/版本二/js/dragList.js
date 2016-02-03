1096fbbdac583a4a2579d14e0d6015a257b5cc20
ichenghaojie <1072095649@qq.com>
2016-02-03 16:34:52 +0800

    re upload

---
+++ js-plugins/dragList/版本二/js/dragList.js
@@ -0,0 +1,173 @@
+/**
+ * 拖拽排序插件
+ * @author chenghaojie
+ */
+
+//获取tagName
+var getTag = function (tagName, oParent) {return (oParent || document).getElementsByTagName(tagName)};
+
+//获取class
+var getClass = function (sClass, oParent) {
+	var aClass = [],
+	i = 0,
+	reClass = new RegExp("(\\s|^)" + sClass + "($|\\s)"),
+	aElement = getTag("*", oParent);
+	for (i = 0; i < aElement.length; i++)reClass.test(aElement[i].className) && aClass.push(aElement[i]);
+	return aClass
+};
+
+var dragList=function(){
+	this.initialize.apply(this,arguments);
+}
+
+dragList.prototype.initialize=function(obj, len){
+	this.box = obj;
+    this.oUl = document.createElement('ul');
+    for(var i=0; i<len; i++){
+    	var aFrag = document.createDocumentFragment();
+    	var oLis = document.createElement('li');
+    	oLis.innerHTML = i;
+    	this.oUl.appendChild(oLis); 
+    }
+    this.box.appendChild(this.oUl);
+    this.oLi = getTag('li', this.oUl);
+    this.pos = [];
+    this.dom = document.documentElement || document.body;
+    this.zIndex = 1;
+
+    this.layOut();
+}
+dragList.prototype.layOut=function(){
+	this.box.style.height = this.box.offsetHeight -2 +'px';
+	this.pos.length = 0;
+	for(var i=0; i<this.oLi.length; i++){
+		this.oLi[i].style.cssText = '';
+		this.oLi[i].index = i;
+		this.oLi[i].style.top = this.getPosition(this.oLi[i]).top + 'px';
+		this.oLi[i].style.left = this.getPosition(this.oLi[i]).left + 'px';
+		this.pos.push( {left:this.getPosition(this.oLi[i]).left, top:this.getPosition(this.oLi[i]).top} );	
+	}
+	for(var i=0; i<this.oLi.length; i++){
+		this.oLi[i].style.position = 'absolute';
+		this.oLi[i].style.margin = 0;
+		this.drag(this.oLi[i]);
+	}
+}
+dragList.prototype.drag=function(obj){
+	var oThis = this;
+	obj.style.cursor = 'move';
+	obj.onmousedown = function(event){
+		var event = event||window.event;
+		var disX = event.clientX - obj.offsetLeft;
+		var disY = event.clientY -obj.offsetTop;
+		var oNear = null;
+        obj.style.zIndex = oThis.zIndex++;
+
+        var borderBox = document.createElement('div');
+        borderBox.className = 'borderBox';
+        oThis.oUl.insertBefore(borderBox, obj);
+        borderBox.style.left = obj.offsetLeft + 'px';
+        borderBox.style.top =obj.offsetTop + 'px';
+		document.onmousemove = function(event){
+			var event = event||window.event;
+			var iL = event.clientX - disX;
+			var iT = event.clientY - disY;
+            
+			var maxL = Math.max(oThis.dom.clientWidth, oThis.dom.scrollWidth);
+			var maxT = Math.max(oThis.dom.clientHeight, oThis.dom.scrollHeight);
+
+			iL < 0 && (iL = 0);
+			iL > maxL && (iL = maxL);
+			iT < 0 && (iT = 0);
+			iT > maxT && (iT = maxT);
+
+			obj.style.left = iL + 'px';
+			obj.style.top = iT + 'px';
+
+            oNear = oThis.findNear(obj);
+
+            if(oNear){
+				var temp = obj.index;
+				obj.index = oNear.index;
+				oNear.index = temp;
+                oThis.doMove(borderBox, oThis.pos[obj.index]);
+                oThis.doMove(oNear, oThis.pos[oNear.index]);
+                
+			}
+            return false;
+		}
+		document.onmouseup = function(){
+			document.onmousemove = null;
+			document.onmouseup = null;
+			
+			oThis.doMove(obj, oThis.pos[obj.index]);
+			borderBox.parentNode.removeChild(borderBox);
+			borderBox = null;
+			obj.releaseCapture && obj.releaseCapture();
+		}
+		this.setCapture && this.setCapture();
+		return false
+	};
+}
+dragList.prototype.findNear=function(obj){
+	var aDis = [];
+	var i = 0; 
+	for(i=0; i<this.oLi.length; i++){
+		this.oLi[i] == obj ? aDis[i] = Number.MAX_VALUE : aDis[i] = this.getDis(obj, this.oLi[i]);
+	}
+	var minDis = Number.MAX_VALUE;
+	var minLi = null;
+	for(i=0; i<aDis.length; i++){
+		minDis > aDis[i] && (minDis = aDis[i], minLi = this.oLi[i]);
+	}
+
+	return this.isButt(obj, minLi) ? minLi : null;
+}
+dragList.prototype.getDis=function(obj1,obj2){
+	var x1 = obj1.offsetLeft + obj1.offsetWidth/2;
+	var y1 = obj1.offsetTop + obj1.offsetHeight/2;
+	var x2 = obj2.offsetLeft + obj2.offsetWidth/2;
+	var y2 = obj2.offsetTop + obj2.offsetHeight/2;
+
+	return dis = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+}
+dragList.prototype.isButt=function(obj1, obj2){
+	var l1 = obj1.offsetLeft;
+	var t1 = obj1.offsetTop;
+	var r1 = l1 + obj1.offsetWidth;
+	var b1 = t1 + obj1.offsetHeight;
+	
+	var l2 = obj2.offsetLeft;
+	var t2 = obj2.offsetTop;
+	var r2 = l2 + obj2.offsetWidth;
+	var b2 = t2 + obj2.offsetHeight;
+	
+	return !(r1 < l2 || b1 < t2 || r2 < l1 || b2 < t1)
+}
+dragList.prototype.doMove=function(obj, position){
+	var oThis = this;
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		var nowL = obj.offsetLeft;
+	    var nowT = obj.offsetTop;
+	    var speedL = (position.left - nowL)/2;
+	    var speedT = (position.top - nowT)/2;
+	    speedL = speedL > 0 ? Math.ceil(speedL) : Math.floor(speedL);
+	    speedT = speedT > 0 ? Math.ceil(speedT) : Math.floor(speedT);
+
+	    ((nowL != position.left) || (nowT != position.top)) ? (obj.style.left = nowL + speedL + 'px',obj.style.top = nowT + speedT + 'px') : 
+	    (clearInterval(obj.timer))
+	},10);
+
+}
+dragList.prototype.getPosition=function(obj){
+	var oTop = obj.offsetTop;
+	var oLeft = obj.offsetLeft;
+	while(obj.offsetParent){
+		oTop += obj.offsetParent.offsetTop;
+		oLeft += obj.offsetParent.offsetLeft;
+		obj = obj.offsetParent;
+	}
+
+	return {top:oTop, left:oLeft}
+}
---
+++ js-plugins/zoomImg/版本一/js/zoomImg.js
@@ -0,0 +1,186 @@
+/**
+ * 图片缩放拖拽插件
+ * @author chenghaojie
+ */
+
+//获取tagName
+var getTag = function (tagName, oParent) {return (oParent || document).getElementsByTagName(tagName)};
+
+//获取class
+var getClass = function (sClass, oParent) {
+    var aClass = [],
+    i = 0,
+    reClass = new RegExp("(\\s|^)" + sClass + "($|\\s)"),
+    aElement = getTag("*", oParent);
+    for (i = 0; i < aElement.length; i++)reClass.test(aElement[i].className) && aClass.push(aElement[i]);
+    return aClass
+};
+var imgZoom = function () {
+    this.initialize.apply(this, arguments)
+};
+    
+imgZoom.prototype.initialize=function(oImg,oSrc){
+    var oThis=this;
+    this.oBox = oImg;
+    
+    this.obj=document.createElement('img');
+    this.obj.src=oSrc;
+    this.oBox.appendChild(this.obj);
+
+    this.zoomIn=document.createElement("button");
+    this.zoomIn.className="zoomIn";
+    this.zoomIn.value="放大";
+    this.zoomIn.innerHTML="放大";
+
+    this.zoomOut=document.createElement('button');
+    this.zoomOut.className="zoomOut";
+    this.zoomOut.value="缩小";
+    this.zoomOut.innerHTML="缩小";
+
+    this.zoomOrg=document.createElement('button');
+    this.zoomOrg.className="zoomOrg";
+    this.zoomOrg.value="还原";
+    this.zoomOrg.innerHTML="还原";
+
+    this.oBox.appendChild(this.zoomIn);
+    this.oBox.appendChild(this.zoomOut);
+    this.oBox.appendChild(this.zoomOrg);
+
+    this.oWidth=this.obj.offsetWidth;
+    this.oHeight=this.obj.offsetHeight;
+    this.changeWidth = this.oWidth*(0.2);
+    this.changeHeight = this.oHeight*(0.2);
+    this.index=0;
+
+    this.zoomIn.onclick=function(){
+        oThis.in();
+    }
+    this.zoomOut.onclick=function(){
+        oThis.out();
+    }
+    this.zoomOrg.onclick=function(){
+        oThis.org();
+    }
+
+    this.drag(this.obj);
+};
+    
+imgZoom.prototype.in=function(){
+    if(this.index > 4){
+        return;
+    }
+    
+    var imgWidth=this.obj.offsetWidth;
+    var imgHeight=this.obj.offsetHeight;
+    var nowL=this.obj.style.left;
+    nowL == '' && (nowL = '0px');
+    var nowT=this.obj.style.top;
+    nowT == '' && (nowT = '0px');
+    
+    this.obj.style.width=imgWidth+this.changeWidth+"px";
+    this.obj.style.height=imgHeight+this.changeHeight+"px";
+    this.obj.style.left = (parseInt(nowL.slice(0,nowL.length-2))+(-(this.changeWidth/2))) +'px';
+    this.obj.style.top = (parseInt(nowT.slice(0,nowT.length-2))+(-(this.changeHeight/2)))+'px';
+    this.index++;
+};
+
+imgZoom.prototype.out=function(){
+    if(this.index < (-3)){
+        return;
+    }
+
+    var imgWidth=this.obj.offsetWidth;
+    var imgHeight=this.obj.offsetHeight;
+    var nowL=this.obj.style.left;
+    nowL == '' && (nowL = '0px');
+    var nowT=this.obj.style.top;
+    nowT == '' && (nowT = '0px');
+    
+    this.obj.style.width=imgWidth-this.changeWidth+"px";
+    this.obj.style.height=imgHeight-this.changeHeight+"px";
+    if(this.index>0){
+        this.index--;
+        var oL = (parseInt(nowL.slice(0,nowL.length-2))+(this.changeWidth/2));
+        var disW = this.obj.offsetWidth - this.oBox.offsetWidth;
+        oL >= 0 && (oL = 0);
+        Math.abs(oL) >= (disW) && (oL = (-(disW)));
+        
+        var oT = (parseInt(nowT.slice(0,nowT.length-2))+(this.changeHeight/2));
+        var disH = this.obj.offsetHeight - this.oBox.offsetHeight
+        oT >= 0 && (oT = 0);
+        Math.abs(oT) >= (disH) && (oT = (-(disH)));
+        
+        this.obj.style.left = oL +'px';
+        this.obj.style.top = oT + 'px';
+        
+    }else{
+        this.index--;
+        var sIndex=Math.abs(this.index);
+        this.obj.style.left = (this.changeWidth/2*sIndex)+'px';
+        this.obj.style.top = (this.changeHeight/2*sIndex)+'px';
+ 
+    }
+};
+
+imgZoom.prototype.drag=function(obj){
+    
+    var oThis = this;
+    var handle = obj;
+    handle.style.cursor = "move";
+    handle.onmousedown=function(event){
+        if(oThis.index<0){
+            handle.style.cursor = "default";
+            return ;
+        }else{
+            handle.style.cursor = "move";
+        }
+        var event=event||window.event;
+        var disX=event.clientX-oThis.oBox.offsetLeft;
+        var disY=event.clientY-oThis.oBox.offsetTop;
+        
+        var nowL=handle.style.left;
+        var nowT=handle.style.top;
+        
+        document.onmousemove=function(event){
+            var event=event||window.event;
+            var iL=event.clientX-oThis.oBox.offsetLeft-disX;
+            var iT=event.clientY-oThis.oBox.offsetTop-disY;
+            
+            var imgW=handle.style.width;
+            var imgH=handle.style.height;
+            var minx = -(parseInt(imgW.slice(0,imgW.length-2))-oThis.oBox.offsetWidth);
+            var minY = -(parseInt(imgH.slice(0,imgH.length-2))-oThis.oBox.offsetHeight);
+
+            var changeX = (parseInt(nowL.slice(0,nowL.length-2))+iL);
+            changeX > 0 && (changeX = 0);
+            changeX < minx && (changeX = minx);
+            
+            var changeY = (parseInt(nowT.slice(0,nowT.length-2))+iT);
+            changeY > 0 && (changeY = 0);
+            changeY < minY && (changeY = minY);
+
+            handle.style.left=changeX+'px';
+            handle.style.top=changeY+'px';
+            
+            return false;
+        }
+        document.onmouseup=function(){
+            document.onmousemove=null;
+            document.onmouseup=null;
+
+            handle.releaseCapture && handle.releaseCapture()
+        }
+
+        this.setCapture && this.setCapture();
+        return false;
+    }
+
+};
+
+imgZoom.prototype.org=function(){
+    this.obj.style.width=this.oWidth+"px";
+    this.obj.style.height=this.oHeight+"px";
+    this.obj.style.left = 0+'px';
+    this.obj.style.top = 0+'px';
+    this.index=0;
+}
